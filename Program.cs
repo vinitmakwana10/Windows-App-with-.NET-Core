afb46776556b891d80663e07b7be40decdb58499
vinitmakwana10 <49344433+vinitmakwana10@users.noreply.github.com>
2020-07-29 00:48:40 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace TextEditor
 {
     static class Program
@@ -16,9 +15,7 @@ namespace TextEditor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-
-       
-    }
+            Application.Run(new Form1());    
+        }
     }
 }
